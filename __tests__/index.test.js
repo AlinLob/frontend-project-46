51593070a5e6d679c2c0b31dbe250214e2b15233
alinlob <alinalobova17@gmail.com>
2023-03-16 10:34:13 +0300

    add __tests__/

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@jest/globals';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+describe('check diff', () => {
+  test('genDiff', () => {
+    const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+    expect((actual)).toBe(actual);
+  });
+});
